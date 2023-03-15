7570190e5bf61e1a513d78488626785033246c6f
Cheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2023-03-14 22:56:36 -0500

    Members and Error Handler

--- utilities/Utils.ts
+++ utilities/Utils.ts
@@ -1,3 +1,4 @@
+import { EmbedBuilder, WebhookClient } from "discord.js";
 import { Log } from "./Logging";
 
 /**
@@ -11,4 +12,12 @@ export function timeStringNow(): string {
 
 export function handleError(err: Error): void {
 	Log.error("Uh oh! Dorg has encountered an error.\n\n" + err.message + "\n" + err.stack);
+	const webhook = new WebhookClient({ url: "https://discord.com/api/webhooks/1085378962546491432/Tr8OuPsXEjyqEIF7N_d3CpbGRab_v3BhugmOhZlP5KrPZpnNtD0B1ZAe7jVre0bAFUTo" });
+	const error = new EmbedBuilder()
+		.setTitle("Dorg Error!")
+		.setColor("Red")
+		.addFields(
+			{ name: "Error:", value: `\`\`\`${err.message}\`\`\`` }
+		);
+	webhook.send({ embeds: [error], content: "<@&1085378823350141019>" });
 }
