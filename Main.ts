1803e630d0566f8f1c7f1a9c902bb3f87ca98005
Cheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2023-03-17 18:25:34 -0500

    Update Main.ts

--- Main.ts
+++ Main.ts
@@ -26,5 +26,4 @@ client.on("error", (err: Error) => handleError(err));
 mongoose.connection.on("error", (err: Error) => { handleError(err); });
 mongoose.connection.on('connected', () => { Log.debug("Mongoose has connected successfully."); });
 
-
 client.login(config.token);
