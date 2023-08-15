6fc24ee3854d8fa2cfb9031760222786de0f73a4
Cheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2023-08-14 22:42:55 -0500

    Update WipeCases.ts

--- commands/slash/configuration/WipeCases.ts
+++ commands/slash/configuration/WipeCases.ts
@@ -1,6 +1,8 @@
-import { Message, PermissionFlagsBits } from "discord.js";
+import { APIButtonComponent, ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, Embed, EmbedBuilder, Message, PermissionFlagsBits } from "discord.js";
 import { client } from "../../../Main";
 import { CommandExecutor, PermissionLevel } from "../../../classes/CommandExecutor";
+import Settings from "../../../schemas/Settings";
+import Case from "../../../schemas/Case";
 
 export default new CommandExecutor()
 	.setName("wipecases")
@@ -10,7 +12,61 @@ export default new CommandExecutor()
 	})
 	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 	.setExecutor(async (interaction) => {
-		const resultMessage: Message = await interaction.reply({ content: "🔃 Calculating...", fetchReply: true });
-		const ping = resultMessage.createdTimestamp - interaction.createdTimestamp;
-		interaction.editReply({ content: `<:check:1069088768830738554> Bot Latency: **${ping}ms**, API Latency: **${client.ws.ping}ms**` });
+		if (!interaction.inCachedGuild()) { interaction.reply({ content: "You must be inside a cached guild to use this command!", ephemeral: true }); return; }
+
+		let row = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Danger)
+					.setCustomId("CONFIRM")
+					.setLabel("Confirm")
+					.setEmoji("🛑")
+			);
+
+		const settings = await Settings.findOne({
+			guildID: interaction.guild.id,
+		});
+
+		const areYouSureEmbed = new EmbedBuilder()
+			.setTitle("Warning!")
+			.setColor("Blurple")
+			.setDescription(`You are about to delete all case files, are you sure you want to do that?`)
+			.setTimestamp();
+		const buttonMessage = await interaction.reply({ embeds: [areYouSureEmbed], components: [row], fetchReply: true });
+
+		const collector = buttonMessage.createMessageComponentCollector({ time: 15000 });
+
+		collector.on('collect', async (buttonInteraction: ButtonInteraction) => {
+			await interaction.deferReply();
+			if (buttonInteraction.user.id !== interaction.user.id) {
+				buttonInteraction.reply({ content: "This is not your button!", ephemeral: true });
+				return;
+			}
+			if (buttonInteraction.customId == "CONFIRM") {
+				if (buttonInteraction.user.id === interaction.user.id) {
+					row = new ActionRowBuilder<ButtonBuilder>()
+						.addComponents(
+							ButtonBuilder.from(buttonMessage.components[0].components[0] as APIButtonComponent).setDisabled(true)
+						);
+
+					const doneEmbed = new EmbedBuilder()
+						.setTitle("Success")
+						.setColor("Blurple")
+						.setDescription(`All case files have been deleted.`)
+						.setTimestamp();
+
+					await interaction.editReply({ embeds: [doneEmbed], components: [row] });
+					await Settings.findOneAndUpdate({
+						guildID: interaction.guild?.id
+					}, {
+						guildSettings: {
+							totalCases: 0
+						}
+					});
+					await Case.deleteMany({
+						guildID: interaction.guild?.id
+					});
+				}
+			}
+		});
 	});
